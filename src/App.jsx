2a4a8f8e4fdc113444d1674cb352e0246f9df5e8
Veeraragavan <veeraragavanreghupathy02@gmail.com>
2025-09-24 16:22:22 +0530

    resolve merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
-import Login from './components/Loginpage/login'
-=======
 import Signup from './components/Sign-up/Signup'
->>>>>>> main
+import Login from './components/Loginpage/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-<Login/>
-=======
     <Signup />
->>>>>>> main
+<Login/>
     </>
   )
 }
